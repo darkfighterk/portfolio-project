f7cab39eefeb5df5aa0664cc3bff07cf71716f0e
Anjana <153971323+darkfighterk@users.noreply.github.com>
2026-06-08 08:10:44 +0530

    Update next.config.ts

--- portfolio-project/next.config.ts
+++ portfolio-project/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/https://github.com/darkfighterk/portfolio-project.git",  
+  basePath: "/portfolio-project",  
     images: {
     unoptimized: true,
   },
